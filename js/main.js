4e92ccbce3ac18084199501c8e77ad4713beb775
yukai1004 <yuka.iida104@gmail.com>
2025-07-11 13:51:37 +0900

    ハンバーガーメニューの左からフレームインを実装

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function(){
   $('#hamburger').on('click',function(){
     console.log('click');
     //$('#hamburger').toggleClass("open");
-    $('.close').toggleClass("open");
+    $('#header-menu').toggleClass("open");  
     //$('#header-menu').slideToggle();
   });
 });
